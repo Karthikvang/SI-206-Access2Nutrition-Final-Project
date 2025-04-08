85d5ccecd844b326df9df90f7cf70cfdeeae2ac7
Anna <annakhou@umich.edu>
2025-04-08 15:47:39 -0400

    Anna- working on getting data into the db i created

--- api_2_fetch.py
+++ api_2_fetch.py
@@ -11,7 +11,6 @@ BASE_URL = 'https://calendarific.com/api/v2'
 #API KEY
 API_KEY = 'MVRNRBvAvQwPU7gbpLmAoypbfnrw9SeT'
 COUNTRY_CODE = 'US'
-COUNTRY_NAME = 'United States'
 YEAR = 2023
 
 #create database
@@ -43,6 +42,37 @@ CREATE TABLE IF NOT EXISTS holidays (
 # Commit changes and close connection
 conn.commit()
 conn.close()
+
+
+
+#i made this function to make request to API to fetch holiday data from my API
+#I did this for the US (united states) and chose to look at year 2023
+
+def fetch_us_holidays(api_key, year=YEAR, country_code=COUNTRY_CODE):
+#first make parameters for a request
+    params = {
+        'api_key': api_key,
+        'country': country_code,
+        'year': year
+    }
+#include parameters with my BASE url to get stuff that I want
+    response = requests.get(BASE_URL, params=params)
+#make info a dictionary
+    data = response.json()
+ # return only the list of holidays from the whole JSON response
+    return data['response']['holidays']
+
+#when I do request make sure to add params when calling base 
+# url the way the example API is within the documentation
+
+
+#NEXT FUNC: Checks if the country already exists in the 'countries' table.
+#If it doesn’t, it inserts it. Returns the country's ID either way.
+
+
+#Inserts up to `limit`(25) number of unique holidays into the database.
+# It uses the 'INSERT OR IGNORE' statement to prevent duplicates
+# based on the UNIQUE constraint on (name, date)
 
 
 
