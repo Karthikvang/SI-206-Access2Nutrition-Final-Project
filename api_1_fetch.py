f666dcc496cd8bbf225bc909cc6c306c5c5bae6a
Srikarthik <karthkv@umich.edu>
2025-04-08 17:36:27 -0400

    Connected to the weatherstack API

--- api_1_fetch.py
+++ api_1_fetch.py
@@ -1,12 +1,20 @@
+import requests
+
+
 weatherstack_key = '42f2c4f28194060340e97220c64eb8af'
 
-def get_wstack_data ():
-    pass
+def get_wstack_data (city):
 
+    # Prints the data of whatever city input into the function
+    url = f"https://api.weatherstack.com/current?access_key={weatherstack_key}"
+    querystring = {"query" : city}
+    response = requests.get(url, params=querystring)
+
+    print(response.json())
 
 
 def main():
-    print("Hello")
+    get_wstack_data('New York')
 
 
 if __name__ == "__main__":
