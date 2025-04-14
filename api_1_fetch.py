9635b2996de6b6949a98d2aa4f20dd8200d7ebdc
Srikarthik <karthkv@umich.edu>
2025-04-13 20:35:44 -0400

    Working on getting weather data, but geocoding failing

--- api_1_fetch.py
+++ api_1_fetch.py
@@ -2,65 +2,41 @@ import requests
 import sqlite3
 
 
-weatherstack_key = '42f2c4f28194060340e97220c64eb8af'
+openweather_key = '6cebbdc5722158ef937fa0f74650b54b'
 
-def get_summer_data (city_str):
+def get_geocode(city_str):
+    limit = 1
+    url = f'''http://api.openweathermap.org/geo/1.0/direct?q={city_str},
+    US-MI,840&limit={limit}&appid={openweather_key}'''
 
+    geocode = requests.get(url).json()
+
+    latitude = geocode[0]['lat']
+    longitude = geocode[0]['lon']
+
+    latitude = round(latitude, 2)
+    longitude = round(longitude,2)
+
+    return (str(latitude), str(longitude))
+    
+
+def get_summer_data (lat, lon):
     # Prints the data of whatever city input into the function
-    url = f"https://api.weatherstack.com/historical?access_key={weatherstack_key}"
-    query = {"query":city_str, "historical_date_start":"2023-06-01", "historical_date_end":"2023-07-31"}
-    summer = requests.get(url, params=query)
+    url = f'''https://history.openweathermap.org/data/2.5/history/city?
+    lat={lat}&lon={lon}&type=hour&start=1687665600&end=1688788800&appid={openweather_key}'''
+    response = requests.get(url)
+    print(response.json())
 
-    return (summer.json())
 
-
-def get_winter_data(city_str):
-    url = f"https://api.weatherstack.com/current?access_key={weatherstack_key}"
-    query = {"query":city_str, "historical_date_start":"2023-11-01", "historical_date_end":"2023-12-31"}
-    winter = requests.get(url, params=query)
-
-    return (winter.json())
+# def get_winter_data(city_str):
+#     pass
 
 def main():
-    summer_info = {}
-    winter_info = {}
-
-    winter_info["Ann Arbor"] = get_winter_data('Ann Arbor')
-    summer_info["Ann Arbor"] = get_summer_data('Ann Arbor')
-    print("Ann Arbor successfully retreived")
-
-    winter_info["Detroit"] = get_winter_data('Detroit')
-    summer_info["Detroit"] = get_summer_data('Detroit')
-    print("Detroit successfully retreived")
-
-    winter_info["Marquette"] = get_winter_data('Marquette')
-    summer_info['Marquette'] = get_summer_data('Marquette')
-    print('Marquette Succesfully retreived')
-
-    winter_info["Lansing"] = get_winter_data('Lansing')
-    summer_info['Lansing'] = get_summer_data('Lansing')
-    print("Lansing successfully retreived")
-
-    winter_info["Traverse City"] = get_winter_data('Traverse City')
-    summer_info['Traverse City'] = get_summer_data('Traverse City')
-    print("Traverse city successfully retreived")
-
-    winter_info["Grand Rapids"] = get_winter_data('Grand Rapids')
-    summer_info['Grand Rapids'] = get_summer_data('Grand Rapids')
-    print("Grand Rapids successfully retreived")
-
-    print(f"WINTER:\n{winter_info}")
-
-    print(f'SUMMER\n{summer_info}')
-
+    aa_geocode = get_geocode('Ann Arbor')
+    get_summer_data(aa_geocode[0], aa_geocode[1])
     conn = sqlite3.connect('A2N.db')
     cur = conn.cursor()
-    # Create the countries table
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS SEASONS (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    country_code TEXT UNIQUE,
-    country_name TEXT)''')
+   
 
 if __name__ == "__main__":
     main()
