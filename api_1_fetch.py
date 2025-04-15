7d84fea2f471d30298a8cb00a6bf6b647b0e55eb
Srikarthik <karthkv@umich.edu>
2025-04-15 10:04:51 -0400

    converted everything to fahrenheit

--- api_1_fetch.py
+++ api_1_fetch.py
@@ -19,8 +19,9 @@ def get_summer_data (lat, lon):
     start = '1719720000' # June 30 2024
     end = '1720411200' # July 8 2024
    
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&appid={openweather_key}&start={start}&end={end}'
+    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&appid={openweather_key}&start={start}&end={end}&units=imperial'
     response = requests.get(url)
+
     return response.json()
 
 
@@ -28,9 +29,47 @@ def get_winter_data(lat, lon):
     start = '1734843600' # December 22 2024
     end = '1735362000' # December 28 2024
 
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&appid={openweather_key}&start={start}&end={end}'
+    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&appid={openweather_key}&start={start}&end={end}&units=imperial'
     response = requests.get(url)
+
+    print(response.json())
     return response.json()
+
+def insert_seasons (season, cur, conn, limit = 25):
+    #counter to track how many holidays been inserted
+    inserted = 0
+    #go thru each holiday in list
+    for city in season:
+        #stop if my limit (of 25) has been reached
+        if inserted >= limit:
+            break
+
+        #this gets name and date of holidays
+        name = holiday['name']
+        date = holiday['date']['iso']
+
+        #check if name and date already in table
+        cur.execute('''
+                    SELECT id FROM holidays WHERE name = ? AND date = ?
+                     ''', (name, date))
+        result = cur.fetchone()
+    #if there is a match then skip (NO DUPLICATES)
+        if result: 
+            continue
+    #insert new non duplicate holiday into table
+        else:
+            cur.execute('''
+                INSERT INTO holidays (name, date)
+                VALUES (?, ?)
+            ''', (name, date))
+    #if inserted then increment counter
+            if cur.rowcount == 1:
+                inserted += 1
+
+    conn.commit()
+    #small print note i have right now to
+    # tell me how many holidays are inserted
+    print(f"{inserted} days inserted.")
 
 def main():
     summer_dict = {}
