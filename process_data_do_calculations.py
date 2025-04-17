07d88668bf9ef390636ca6c06914019f15760388
Srikarthik <karthkv@umich.edu>
2025-04-16 20:36:28 -0400

    instructions for how to do calculations for temperatures

--- process_data_do_calculations.py
+++ process_data_do_calculations.py
@@ -1,1 +1,5 @@
-# For calculating the average temperature for a city for a certain month
+# For calculating the average temperature for a city for a specific month:
+# - Select the city
+# - Select the Month
+# - get the temps for the whole month
+# - get the average (sum of temps / number of days)
