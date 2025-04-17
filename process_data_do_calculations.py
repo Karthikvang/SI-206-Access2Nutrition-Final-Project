9e4d80651f08139bdac430089e8562dcfdca5c8c
Srikarthik <karthkv@umich.edu>
2025-04-17 11:01:39 -0400

    code modified by karthik

--- process_data_do_calculations.py
+++ process_data_do_calculations.py
@@ -1,23 +1,42 @@
 import sqlite3
 
-# For calculating the average temperature for a city for a specific month:
-# - Select the city
-# - Select the Month
-# - get the temps for the whole month
-# - get the average (sum of temps / number of days)
 conn = sqlite3.connect('A2N.db')
 cur = conn.cursor()
 
-def warmest_month_aa():
-    aa_monthly_avg = cur.execute("""SELECT months.month, AVG(temperatures.temp) AS avg_temp
+def monthly_averages():
+    return_dict = {}
+    sf_monthly_avg = cur.execute("""SELECT months.month, AVG(temperatures.temp) AS avg_temp
                          FROM temperatures JOIN months ON temperatures.month_id = months.id 
                          JOIN city ON temperatures.city_id = city.id
                          WHERE city.city_name = ?
-                         GROUP BY months.id""", ('Ann Arbor',))
-    print(aa_monthly_avg.fetchall())
+                         GROUP BY months.id""", ('San Francisco',)).fetchall()
+    return_dict['San Francisco'] = sf_monthly_avg
+
+    dt_monthly_avg = cur.execute("""SELECT months.month, AVG(temperatures.temp) AS avg_temp
+                         FROM temperatures JOIN months ON temperatures.month_id = months.id 
+                         JOIN city ON temperatures.city_id = city.id
+                         WHERE city.city_name = ?
+                         GROUP BY months.id""", ('Detroit',)).fetchall()
+    return_dict['Detroit'] = dt_monthly_avg
+
+    ny_monthly_avg = cur.execute("""SELECT months.month, AVG(temperatures.temp) AS avg_temp
+                         FROM temperatures JOIN months ON temperatures.month_id = months.id 
+                         JOIN city ON temperatures.city_id = city.id
+                         WHERE city.city_name = ?
+                         GROUP BY months.id""", ('New York',)).fetchall()
+    return_dict['New York'] = ny_monthly_avg
+
+    dl_monthly_avg = cur.execute("""SELECT months.month, AVG(temperatures.temp) AS avg_temp
+                         FROM temperatures JOIN months ON temperatures.month_id = months.id 
+                         JOIN city ON temperatures.city_id = city.id
+                         WHERE city.city_name = ?
+                         GROUP BY months.id""", ('Dallas',)).fetchall()
+    return_dict['Dallas'] = dl_monthly_avg
+
+    return return_dict
 
 def main():
-    warmest_month_aa()
+    monthly_averages()
 
 if __name__ == "__main__":
     main()
